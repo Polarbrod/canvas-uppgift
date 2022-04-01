3bc53fdf109ff082b2b4f307740dc112345835d8
Adam Nilsson <adam.nilsson@skola.taby.segit config --global user.name Adam>
2022-04-01 10:05:20 +0200

    Test

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ canvas.style.height = "100%";
 // Genom variabeln c kommer man åt det mesta som
 // med canvas att göra
 const c = canvas.getContext("2d");
-const midX = canvas.width / 2;
+const midX = canvas.width / 2; 
 const midY = canvas.height / 2;
 
 // Matar ut canvas dimensioner till konsolen, pröva att
